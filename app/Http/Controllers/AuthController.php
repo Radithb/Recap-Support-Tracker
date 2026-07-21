59822f27edb0d2269f0b97a711206ed88aae4fa5
AthaFathiMurtadha <athafathiys@gmail.com>
2026-07-21 18:41:44 +0700

    Fix: Perbaiki bahasa pada notifikasi saat mengubah preferensi bahasa

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
@@ -255,8 +256,13 @@ class AuthController extends Controller
         ]);
 
         $user = Auth::user();
-        $user->locale = $validated['locale'];
-        $user->save();
+        if ($user) {
+            $user->locale = $validated['locale'];
+            $user->save();
+        }
+        session()->put('locale', $validated['locale']);
+
+        App::setLocale($validated['locale']);
 
         return back()->with('success', __('messages.bahasa_updated'));
     }
