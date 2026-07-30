aac76527e0147b0c73eed59475ce4a344c5572b5
Radith <mradithn@gmail.com>
2026-07-30 19:26:36 +0700

    fix: Menghapus syntax error di file migrasi

--- database/migrations/2026_07_30_192500_create_aplikasi_implementasi_table.php
+++ database/migrations/2026_07_30_192500_create_aplikasi_implementasi_table.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -8,17 +8,17 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('aplikasi_implementasi', function (Blueprint \) {
-            \->id();
-            \->foreignId('implementasi_id')->constrained('implementasi_koperasi')->onDelete('cascade');
-            \->foreignId('aplikasi_id')->constrained('master_aplikasis')->onDelete('cascade');
-            \->timestamps();
+        Schema::create('aplikasi_implementasi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('implementasi_id')->constrained('implementasi_koperasi')->onDelete('cascade');
+            $table->foreignId('aplikasi_id')->constrained('master_aplikasis')->onDelete('cascade');
+            $table->timestamps();
         });
 
         // Hapus foreign key lama dari implementasi_koperasi (jika ada) dan bolehkan null
-        Schema::table('implementasi_koperasi', function (Blueprint \) {
-            // \->dropForeign(['aplikasi_id']); // Kita skip ini dulu karena di infinityfree sering masalah. Biarkan kolom lama tetap ada tapi nullable
-            \->unsignedBigInteger('aplikasi_id')->nullable()->change();
+        Schema::table('implementasi_koperasi', function (Blueprint $table) {
+            // $table->dropForeign(['aplikasi_id']); // Kita skip ini dulu karena di infinityfree sering masalah. Biarkan kolom lama tetap ada tapi nullable
+            $table->unsignedBigInteger('aplikasi_id')->nullable()->change();
         });
     }
 
